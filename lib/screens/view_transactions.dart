aacc0f9e182201ce293d098ccf83303f8eebe0d2
vashmeow <isanjuanivan@gmail.com>
2026-01-09 07:17:40 +0800

    added selectedBorrower, currently adding null safe operator

--- lib/screens/view_transactions.dart
+++ lib/screens/view_transactions.dart
@@ -1,5 +1,7 @@
+import 'package:debt_payment_tracker_app/constants/transaction_type.dart';
 import 'package:debt_payment_tracker_app/models/borrower_account.dart';
 import 'package:debt_payment_tracker_app/models/general_ledger.dart';
+import 'package:debt_payment_tracker_app/screens/add_borrower.dart';
 import 'package:flutter/material.dart';
 
 class ViewTransactions extends StatefulWidget {
@@ -12,20 +14,60 @@ class ViewTransactions extends StatefulWidget {
 }
 
 class _ViewTransactionsState extends State<ViewTransactions> {
+  BorrowerAccount? selectedAccount;
   @override
   Widget build(BuildContext context) {
     var borrowerAccounts = widget.generalLedger.allBorrowers;
     return Scaffold(
       appBar: AppBar(title: Text('View Transactions')),
-      body: Column(
-        children: [
-          DropdownMenu(
-            dropdownMenuEntries: borrowerAccounts.map((e) {
-              return DropdownMenuEntry(value: e, label: '${e.name}');
-            }).toList(),
-          ),
-        ],
-      ),
+      body: (borrowerAccounts.isEmpty == true
+          ? AlertDialog(
+              title: Text('No Borrower Account Found.'),
+              content: Text(
+                'Do you want to be directed to the borrower account creation screen?',
+              ),
+              actions: [
+                TextButton(
+                  onPressed: () async {
+                    Navigator.of(context)
+                        .push(
+                          MaterialPageRoute(
+                            builder: (_) => AddBorrower(
+                              generalLedger: widget.generalLedger,
+                            ),
+                          ),
+                        )
+                        .then((newBorrower) {
+                          if (newBorrower != null) {}
+                          setState(() {
+                            Navigator.pop(context, newBorrower);
+                          });
+                        });
+                  },
+                  child: Text('Yes'),
+                ),
+                TextButton(
+                  onPressed: () {
+                    Navigator.pop(context);
+                  },
+                  child: Text('No'),
+                ),
+              ],
+            )
+          : Column(
+              children: [
+                DropdownMenu(
+                  dropdownMenuEntries: borrowerAccounts.map((e) {
+                    return DropdownMenuEntry(value: e, label: '${e.name}');
+                  }).toList(),
+                  onSelected: (BorrowerAccount? value) {
+                    setState(() {
+                      selectedAccount = value;
+                    });
+                  },
+                ),
+              ],
+            )),
     );
   }
 }
